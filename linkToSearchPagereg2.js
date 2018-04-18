7b9298fab9d990d5a0792b6ea192bce37f2c7f9e
Achillessanger <1359848409@qq.com>
2018-04-18 21:02:03 +0800

    更新了画廊切换

---
+++ linkToSearchPagereg2.js
@@ -0,0 +1,22 @@
+
+document.getElementById('searchbar-input4').onkeydown=function(event){
+
+    var e = event || window.event || arguments.callee.caller.arguments[0];
+    var keywords = document.getElementById('searchbar-input4').value;
+    if(e && e.keyCode==13){ // enter 键
+        window.location='searchpage-registered.html';
+        if(keywords == "123"){
+            document.getElementById('no-search-result').classList.remove('hide');
+            document.getElementById('artworksArea').classList.add('hide');
+            document.getElementById('bottom-dir').classList.add('hide');
+            document.getElementById('searchcontent').classList.add('hide');
+
+        }else {
+            document.getElementById('searchcontent').classList.remove('hide');
+            document.getElementById('searchcontent').innerHTML=keywords;
+            document.getElementById('no-search-result').classList.add('hide');
+            document.getElementById('artworksArea').classList.remove('hide');
+            document.getElementById('bottom-dir').classList.remove('hide');
+        }
+    }
+};
